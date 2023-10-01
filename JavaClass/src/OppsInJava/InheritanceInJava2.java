711061d0ef4d52e0a9670c61114575adb7146a0f
Rajdeepak <rajdeepakvishwakarma24@gmail.com>
2023-10-02 01:34:10 +0530

    OOPS IN JAVA excluded abstraction n poly

---
+++ JavaClass/src/OppsInJava/InheritanceInJava2.java
@@ -0,0 +1,24 @@
+package OppsInJava;
+
+public class InheritanceInJava2 extends InheritanceInJava1 {
+	
+	public void inherited2(){
+		System.out.println("Inhereted class or dericed classs ");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		InheritanceInJava2 object = new InheritanceInJava2();
+		object.inherited2();
+		object.inherited1();
+	/////////////////
+//		hw of 2/10/23
+//		Multilevel Inheritance
+//		Hierarchical Inheritance
+//		Hybrid Inheritance
+//		https://www.geeksforgeeks.org/inheritance-in-java/
+		
+
+	}
+
+}
